483da521e7bfd359b9a610561d61f1926ed4b7cc
Min Cheng <simonmin777@gmail.com>
2019-01-27 14:04:12 -0800

    add print statement

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from sample import Grid
 
 def test_init_01():
+    print('In test 01')
     x = Grid()
     assert x.is_valid() is False
     y = Grid([1, 2, 3, 4, 5, 6, 7, 8, 9])
     assert y.is_valid() is True
 
 def dest_init_02():
+    print('In test 02')
     x = Grid()
     y = Grid([1, 2, 3, 4, 5, 6, 7, 8, 9])
     assert str(x) == '0, 0, 0, 0, 0, 0, 0, 0, 0'
